9bfef5e83476ca36820778af13a0b32f68204940
Upo <hi@upo.one>
2018-12-20 10:30:00 +0000

    Terraform Version update

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -3,7 +3,7 @@ dockerImage=eu.gcr.io/upodroid/python-server
 cd $WORKSPACE/terraform
 ## Init 
 sudo apt install unzip ruby-full -y
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.11_linux_amd64.zip
 unzip terr*.zip
 sudo mv terraform /usr/local/bin/
 sleep 5
@@ -12,5 +12,6 @@ terraform --version
 ## Terraform
 /snap/bin/gsutil cp gs://upo-scripts/terraform/terraform.json .
 terraform init
+terraform refresh
 terraform plan
 terraform apply -var gcrimage=${dockerImage}:Build-${BUILD_NUMBER} -var instancename=python-web-build-${BUILD_NUMBER} --auto-approve
